8655ae0c39efaf3eacd34b4f7305971b2218e77c
gbaranski <root@gbaranski.com>
2021-06-21 01:09:56 +0200

    chore: add more benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,8 +1,8 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use tokio::runtime::Runtime;
-use rand::{RngCore, Rng};
+use rand::{Rng, RngCore};
 use serde::{Deserialize, Serialize};
 use szafka::Szafka;
+use tokio::runtime::Runtime;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 struct Something {
@@ -12,25 +12,35 @@ struct Something {
 
 impl Something {
     pub fn random() -> Self {
-
-    let mut rng = rand::thread_rng();
-    let mut name = [0; 16];
-    rng.fill_bytes(&mut name);
-    let name = hex::encode(name);
-    Self {
-        name,
-        id: rng.gen(),
+        let mut rng = rand::thread_rng();
+        let mut name = [0; 16];
+        rng.fill_bytes(&mut name);
+        let name = hex::encode(name);
+        Self {
+            name,
+            id: rng.gen(),
+        }
     }
-}
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let runtime = Runtime::new().expect("create runtime fail");
+    let szafka = Szafka::new("/tmp/szafka-benches");
+
     c.bench_function("save", |b| {
         b.to_async(&runtime).iter(|| async {
-            let szafka = Szafka::new("/tmp/szafka-benches");
             let something = Something::random();
             szafka.save(&something).await.unwrap();
+        })
+    });
+
+    let something = Something::random();
+    runtime.block_on(async {
+        szafka.save(&something).await.unwrap();
+    });
+    c.bench_function("get", |b| {
+        b.to_async(&runtime).iter(|| async {
+            assert_eq!(szafka.get().await.unwrap(), something);
         })
     });
 }
